e7a76f3aabac898817691fb7e97c9d6d362cdb50
hkey0 <81494947+hkey0@users.noreply.github.com>
2024-07-30 21:43:31 +0300

    (feat) make it mainnet compatible

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 // import { ethers } from 'https://cdn.ethers.io/lib/ethers-5.2.umd.min.js';
 
+async function getEclipseBalance(address) {
+    const response = await fetch('https://mainnetbeta-rpc.eclipse.xyz/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            jsonrpc: '2.0',
+            id: 1,
+            method: 'getBalance',
+            params: [address]
+        })
+    });
+
+    const { result: { value } } = await response.json();
+    return value / 1_000_000_000;
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-
+    document.getElementById("balance").value = `Your will see your Eclipse balance here.`
     var web3 = new Web3(Web3.givenProvider);
+    window.valid_address = false;
 
+	document.getElementById("eclipse-wallet").addEventListener("input", async function() {
+		let data = document.getElementById("eclipse-wallet").value;
+		console.log(data)
+		if (data.length >= 41) {
+			let balance = await getEclipseBalance(data)
+				.then(b => {
+					console.log("Balance is ", b)
+					window.valid_address = true;
+					document.getElementById("balance").value = `Your Eclipse balance is: ${b}`
+					document.getElementById("bridge-button").disabled = null;
+				})
+				.catch(err => {
+					window.valid_address = false;
+					document.getElementById("bridge-button").disabled = true;
+				});
+		}
+	});
 
     async function testi() {
         connectBtn = document.getElementById("connect-button");
         walletArea = document.getElementById("sender-eth-addr")
 
-        // switch to sepolia
+        // switch to mainnet
         await window.ethereum.request({
             method: 'wallet_switchEthereumChain',
-              params: [{ chainId: web3.utils.toHex(11155111) }],
+              params: [{ chainId: web3.utils.toHex(1) }],
             });
 
         await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -23,13 +57,13 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     testi();
-    async function deposit() {
 
+    async function deposit() {
         let chain = await window.ethereum.chainId
-        if (chain != 0xaa36a7) {
+        if (chain != 0x1) {
          await window.ethereum.request({
             method: 'wallet_switchEthereumChain',
-              params: [{ chainId: web3.utils.toHex(11155111) }],
+              params: [{ chainId: web3.utils.toHex(1) }],
             });
         }
 
@@ -38,8 +72,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
         console.log("Eclipse addr: ", eclipseAddr, amountinWei)
-
-
         var contract = new web3.eth.Contract([
             {
                 "inputs": [
@@ -59,17 +91,16 @@ document.addEventListener('DOMContentLoaded', function () {
                 "stateMutability": "payable",
                 "type": "function"
             }
-        ], "0x11b8db6bb77ad8cb9af09d0867bb6b92477dd68e")
+        ], "0x83cb71d80078bf670b3efec6ad9e5e6407cd0fd1")
 
 
         let ecipseAddrParam = ethers.utils.hexlify(ethers.utils.base58.decode(eclipseAddr))
-
         contract.methods.deposit(ecipseAddrParam, amountinWei).send({
                 from: (await web3.eth.getAccounts())[0],
-                value: amountinWei, // + 231 * (10**9),
-                gasLimit: "300031"
+                value: amountinWei,
+                // gasLimit: "300031"
             }).once("transactionHash", (hash) => {
-                window.open("https://sepolia.etherscan.io/tx/" + hash)
+                window.open("https://etherscan.io/tx/" + hash)
                 console.log("hash", hash)
             }).then(function (result) { console.log(result) })
     }
